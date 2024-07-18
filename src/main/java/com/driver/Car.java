04e14ea1c39d48f58189e3666bb19218b233d1e6
Vijaysirvi11 <vkc66183@gmail.com>
2024-07-18 15:51:06 +0530

    pushing code

--- src/main/java/com/driver/Car.java
+++ src/main/java/com/driver/Car.java
@@ -10,16 +10,24 @@ public class Car extends Vehicle {
     private int seats;
 
     public Car(String name, int wheels, int doors, int gears, boolean isManual, String type, int seats) {
-        //Hint: Car extends Vehicle
+        super(name);
+        this.wheels = wheels;
+        this.doors = doors;
+        this.gears = gears;
+        this.isManual = isManual;
+        this.type = type;
+        this.seats = seats;
+        this.currentGear = 1; // Initial gear when car is started
     }
 
-    public void changeGear(int newGear){
-
+    public void changeGear(int newGear) {
+        this.currentGear = newGear;
         System.out.println("changeGear method called - The gear is changed to: " + currentGear);
     }
 
-    public void changeSpeed(int newSpeed, int newDirection){
-
-        System.out.println("changeSpeed method called - The speed is changed to: " + newSpeed + ", and the direction is changed to: " + newDirection + " degrees");
+    @Override
+    public void move(int speed, int direction) {
+        super.move(speed, direction); // Calls the move method of superclass (Vehicle)
+        System.out.println("Car is moving at speed " + speed + " in direction " + direction + " degrees.");
     }
 }
